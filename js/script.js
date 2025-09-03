c371079c79d712baee8fadbe168cc0d60c82a310
Lindsteez <lindgren.axel@hotmail.com>
2025-09-03 08:35:13 +0200

    Nya förädniingar med erfarenheter

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let slideIndex = 0;
+const slides = document.getElementsByClassName("carousel-slide");
+
+function showSlide(index) {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[index].style.display = "block";
+}
+
+function changeSlide(n) {
+  slideIndex += n;
+  if (slideIndex >= slides.length) slideIndex = 0;
+  if (slideIndex < 0) slideIndex = slides.length - 1;
+  showSlide(slideIndex);
+}
+
+showSlide(slideIndex);
