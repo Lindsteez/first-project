3566137274154e26150e3d86e301d766fb792064
Lindsteez <lindgren.axel@hotmail.com>
2025-09-19 11:55:45 +0200

    fixed image sizes , and fixed mobile performance to 100 by image size for mobile and faster loading google fonts

--- js/script.js
+++ js/script.js
@@ -33,3 +33,20 @@ document.querySelectorAll('.flip-box').forEach(box => {
     box.classList.toggle('flipped');
   });
 });
+
+window.onscroll = function() {
+    const btn = document.getElementById("scrollTopBtn");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  };
+
+  // Scrolla till toppen när man klickar
+  document.getElementById("scrollTopBtn").onclick = function() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
